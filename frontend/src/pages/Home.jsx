99da47de33d5220dcef6d6230247e50665b02d9d
romarioreyes45 <romario.reyes.maldonado@gmail.com>
2024-06-24 21:07:18 -0600

    home update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../api";
-import Note from "../components/Note"
-import "../styles/Home.css"
+import Note from "../components/Note";
+import "../styles/Home.css";
 
 function Home() {
     const [notes, setNotes] = useState([]);
@@ -47,39 +47,82 @@ function Home() {
     };
 
     return (
-        <div>
-            <div>
-                <h2>Notes</h2>
-                {notes.map((note) => (
-                    <Note note={note} onDelete={deleteNote} key={note.id} />
-                ))}
+        <div className="container">
+            <div className="sidebar">
+                <nav>
+                    <h2>TUKEY</h2>
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <a href="#">Blog</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">
+                                <img src="/src/assets/icons/post.png" alt="Projects" />
+                                Projects
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">
+                                <img src="/src/assets/icons/newspaper.png" alt="News" />
+                                News
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">Browse New Channels</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">
+                                <img src="/src/assets/icons/social-network-1.png" alt="Social Media" />
+                                Social Media
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">
+                                <img src="/src/assets/icons/share.png" alt="Share" />
+                                Share profile
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <img src="/src\assets\icons\Tukey_fron.png" alt="Pavito" />
             </div>
-            <h2>Create a Note</h2>
-            <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <label htmlFor="content">Content:</label>
-                <br />
-                <textarea
-                    id="content"
-                    name="content"
-                    required
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                ></textarea>
-                <br />
-                <input type="submit" value="Submit"></input>
-            </form>
+            <div className="content">
+                <div className="header">
+                    <h2>Notes</h2>
+                        <div className="search-container">
+                    <input type="text" className="search-input" placeholder="Search" />
+                </div>
+            </div>
+                <div className="notes-grid">
+                    {notes.map((note) => (
+                        <Note note={note} onDelete={deleteNote} key={note.id} />
+                    ))}
+                </div>
+                <h2>Create a Note</h2>
+                <form onSubmit={createNote}>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                    <label htmlFor="content">Content:</label>
+                    <textarea
+                        id="content"
+                        name="content"
+                        required
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    ></textarea>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 }
 
 export default Home;
+
