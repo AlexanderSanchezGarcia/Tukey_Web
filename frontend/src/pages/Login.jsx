2844a3e269e6d6322c09f01e2a05979b15459325
barrancosantiagofede <federico45168@gmail.com>
2024-06-25 11:45:48 -0600

    Register form update

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Form from '../components/Form';
+import '../styles/login.css';
+
+function Login() {
+    return (
+        <div className="login-page">
+            <nav className="navbar">
+                <div className="logo">TUKEY</div>
+                <ul className="nav-links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/join">Join</a></li>
+                </ul>
+            </nav>
+            <div className="content">
+                <div className="image-placeholder">
+                    {}
+                </div>
+                <div className="login-form">
+                    <Form route="/api/token/" method="login" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
+
